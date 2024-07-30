1594326057de337f39a382cea8ef01694a3476f7
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-07-30 21:33:18 +0200

    Added ProfileResolver.java

--- src/main/java/eu/unchat/uhc/demonslayer/DSPlugin.java
+++ src/main/java/eu/unchat/uhc/demonslayer/DSPlugin.java
@@ -5,11 +5,15 @@ import dev.rollczi.litecommands.bukkit.LiteBukkitFactory;
 import eu.unchat.uhc.API;
 import eu.unchat.uhc.demonslayer.command.DemonSlayerCommand;
 import eu.unchat.uhc.demonslayer.command.permission.DSPermissionHandler;
+import eu.unchat.uhc.demonslayer.command.resolver.ProfileResolver;
 import eu.unchat.uhc.demonslayer.command.usage.DSUsageHandler;
 import eu.unchat.uhc.demonslayer.command.validator.role.HasRole;
 import eu.unchat.uhc.demonslayer.command.validator.role.HasRoleValidator;
+import eu.unchat.uhc.demonslayer.command.validator.team.HasTeam;
+import eu.unchat.uhc.demonslayer.command.validator.team.HasTeamValidator;
 import eu.unchat.uhc.demonslayer.role.DSRoleHandler;
 import eu.unchat.uhc.demonslayer.team.DSTeamHandler;
+import eu.unchat.uhc.profile.IProfile;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -33,9 +37,11 @@ public final class DSPlugin extends JavaPlugin {
         Bukkit.getScheduler().runTaskLater(this, () -> {
             this.liteCommands = LiteBukkitFactory.builder()
                     .commands(new DemonSlayerCommand())
-                    .annotations(config ->
-                            config.validator(Player.class, HasRole.class, new HasRoleValidator())
-                    )
+                    .annotations(config -> {
+                        config.validator(Player.class, HasRole.class, new HasRoleValidator());
+                        config.validator(Player.class, HasTeam.class, new HasTeamValidator());
+                    })
+                    .argument(IProfile.class, new ProfileResolver())
                     .invalidUsage(new DSUsageHandler())
                     .missingPermission(new DSPermissionHandler())
                     .build();
---
+++ src/main/java/eu/unchat/uhc/demonslayer/command/resolver/ProfileResolver.java
@@ -0,0 +1,39 @@
+package eu.unchat.uhc.demonslayer.command.resolver;
+
+import dev.rollczi.litecommands.argument.Argument;
+import dev.rollczi.litecommands.argument.parser.ParseResult;
+import dev.rollczi.litecommands.argument.resolver.ArgumentResolver;
+import dev.rollczi.litecommands.invocation.Invocation;
+import dev.rollczi.litecommands.suggestion.SuggestionContext;
+import dev.rollczi.litecommands.suggestion.SuggestionResult;
+import eu.unchat.uhc.API;
+import eu.unchat.uhc.profile.IProfile;
+import org.bukkit.command.CommandSender;
+
+public final class ProfileResolver extends ArgumentResolver<CommandSender, IProfile> {
+    @Override
+    protected ParseResult<IProfile> parse(Invocation<CommandSender> invocation, Argument<IProfile> argument, String s) {
+        IProfile found = API.get()
+                .getProfileHandler()
+                .getAllProfiles().stream()
+                .filter(profile -> profile.getName().equalsIgnoreCase(s))
+                .findFirst()
+                .orElse(null);
+
+        if (found == null) {
+            return ParseResult.failure("&cProfile with name \"" + s + "\" does not exist");
+        }
+
+        return ParseResult.success(found);
+    }
+
+    @Override
+    public SuggestionResult suggest(Invocation<CommandSender> invocation, Argument<IProfile> argument, SuggestionContext context) {
+        return API.get()
+                .getProfileHandler()
+                .getAllProfiles()
+                .stream()
+                .map(IProfile::getName)
+                .collect(SuggestionResult.collector());
+    }
+}
