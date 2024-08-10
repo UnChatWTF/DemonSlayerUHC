9b5436ebeb2171824c4867dc7cca30933c4e87ea
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-10 17:34:45 +0200

    Added Nezuko

---
+++ src/main/java/eu/unchat/uhc/demonslayer/power/slayer/nezuko/BurningBloodPower.java
@@ -0,0 +1,45 @@
+package eu.unchat.uhc.demonslayer.power.slayer.nezuko;
+
+import eu.unchat.uhc.power.AbstractItemPower;
+import eu.unchat.uhc.util.CC;
+import eu.unchat.uhc.util.ItemBuilder;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+public final class BurningBloodPower extends AbstractItemPower {
+    private final String name;
+    private final int initialCooldown, initialUses;
+    private final ClickType clickType;
+
+    public BurningBloodPower() {
+        this.name = "&c&lBURNING BLOOD";
+        this.initialCooldown = 15 * 60;
+        this.initialUses = 2;
+        this.clickType = ClickType.RIGHT_CLICK;
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return new ItemBuilder(Material.BLAZE_POWDER)
+                .name(name + " &8" + CC.SQUARE + " &7Clic-droit")
+                .lore(
+                        "",
+                        "&7Vos coups sont &6enflammés&7, cependant,",
+                        "&7si vous &6enflammez &7un &aslayer&7, il sera alors",
+                        "&dsoigné &7du même nombre de coeurs qu'il était",
+                        "&7censé perdre.",
+                        "&7Si vous enflammez &aTanjiro&7, il obtiendra",
+                        "&c+10% &7de &cForce &7supplémentaire."
+                )
+                .glowing(true)
+                .asItemStack();
+    }
+
+    @Override
+    public boolean onClick(Player player) {
+        return true;
+    }
+}
--- src/main/java/eu/unchat/uhc/demonslayer/role/defaults/slayer/NezukoRole.java
+++ src/main/java/eu/unchat/uhc/demonslayer/role/defaults/slayer/NezukoRole.java
@@ -1,26 +1,29 @@
 package eu.unchat.uhc.demonslayer.role.defaults.slayer;
 
+import eu.unchat.uhc.demonslayer.power.slayer.nezuko.BurningBloodPower;
 import eu.unchat.uhc.demonslayer.team.defaults.SlayerTeam;
 import eu.unchat.uhc.profile.IProfile;
 import eu.unchat.uhc.role.AbstractRole;
 import eu.unchat.uhc.role.Role;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-@Role(name = "Nezuko", identifier = "nezuko", team = SlayerTeam.class)
+@Role(name = "Nezuko", identifier = "nezuko", team = SlayerTeam.class, material = Material.SPIDER_EYE)
 public final class NezukoRole extends AbstractRole {
     public NezukoRole() {
         registerKnownRole(TanjiroRole.class);
+        registerPower(new BurningBloodPower());
     }
 
     @Override
     public void onDay(Player player) {
         IProfile profile = IProfile.of(player.getUniqueId());
-        profile.setStrengthBuffer(profile.getStrengthBuffer() - 20);
+        profile.setStrengthBuffer(profile.getStrengthBuffer() - 15);
     }
 
     @Override
     public void onNight(Player player) {
         IProfile profile = IProfile.of(player.getUniqueId());
-        profile.setStrengthBuffer(profile.getStrengthBuffer() + 20);
+        profile.setStrengthBuffer(profile.getStrengthBuffer() + 15);
     }
 }
