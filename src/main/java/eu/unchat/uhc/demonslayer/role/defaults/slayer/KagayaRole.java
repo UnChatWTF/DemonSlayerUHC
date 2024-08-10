c9499456b98b906f240a3c5f310d13f9f5204ada
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-10 17:37:44 +0200

    Added Kagaya

---
+++ src/main/java/eu/unchat/uhc/demonslayer/role/defaults/slayer/KagayaRole.java
@@ -0,0 +1,10 @@
+package eu.unchat.uhc.demonslayer.role.defaults.slayer;
+
+import eu.unchat.uhc.demonslayer.team.defaults.SlayerTeam;
+import eu.unchat.uhc.role.AbstractRole;
+import eu.unchat.uhc.role.Role;
+import org.bukkit.Material;
+
+@Role(name = "Kagaya", identifier = "kagaya", team = SlayerTeam.class, material = Material.EYE_OF_ENDER)
+public final class KagayaRole extends AbstractRole {
+}
