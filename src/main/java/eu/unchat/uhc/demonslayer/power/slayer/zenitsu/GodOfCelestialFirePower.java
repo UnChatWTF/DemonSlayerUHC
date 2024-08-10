fb7cb783aa3db49a0baf47e6a25c6a4117a3c8e8
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-10 17:34:33 +0200

    Added Zenitsu

---
+++ src/main/java/eu/unchat/uhc/demonslayer/power/slayer/zenitsu/GodOfCelestialFirePower.java
@@ -0,0 +1,40 @@
+package eu.unchat.uhc.demonslayer.power.slayer.zenitsu;
+
+import eu.unchat.uhc.power.AbstractItemPower;
+import eu.unchat.uhc.util.ItemBuilder;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+public final class GodOfCelestialFirePower extends AbstractItemPower {
+    private final String name;
+    private final int initialCooldown, initialUses;
+    private final ClickType clickType;
+
+    public GodOfCelestialFirePower() {
+        this.name = "&a&lGOD OF CELESTIAL FIRE";
+        this.initialCooldown = 0;
+        this.initialUses = 1;
+        this.clickType = ClickType.RIGHT_CLICK;
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return new ItemBuilder(Material.BLAZE_POWDER)
+                .name(name)
+                .lore(
+                        "&7Des &eéclairs &7apparaissent sur les joueurs dans",
+                        "&7un rayon de 30 blocs, infligeant &c2❤ &7de dégâts",
+                        "&7les &6enflammant &7également durant 8 secondes."
+                )
+                .glowing(true)
+                .asItemStack();
+    }
+
+    @Override
+    public boolean onClick(Player player) {
+        return true;
+    }
+}
--- src/main/java/eu/unchat/uhc/demonslayer/role/defaults/slayer/ZenitsuRole.java
+++ src/main/java/eu/unchat/uhc/demonslayer/role/defaults/slayer/ZenitsuRole.java
@@ -1,25 +1,20 @@
 package eu.unchat.uhc.demonslayer.role.defaults.slayer;
 
-import eu.unchat.uhc.API;
-import eu.unchat.uhc.actionbar.IActionBar;
-import eu.unchat.uhc.demonslayer.DSPlugin;
+import eu.unchat.uhc.demonslayer.power.slayer.zenitsu.DivineSpeedPower;
+import eu.unchat.uhc.demonslayer.power.slayer.zenitsu.GodOfCelestialFirePower;
 import eu.unchat.uhc.demonslayer.team.defaults.SlayerTeam;
-import eu.unchat.uhc.power.AbstractItemPower;
+import eu.unchat.uhc.power.AbstractParentPower;
 import eu.unchat.uhc.profile.IProfile;
 import eu.unchat.uhc.role.AbstractRole;
 import eu.unchat.uhc.role.Role;
-import eu.unchat.uhc.util.ItemBuilder;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-@Role(name = "Zenitsu", identifier = "zenitsu", team = SlayerTeam.class)
+@Role(name = "Zenitsu", identifier = "zenitsu", team = SlayerTeam.class, material = Material.BLAZE_ROD)
 public final class ZenitsuRole extends AbstractRole {
     public ZenitsuRole() {
-        registerPower(new DivineSpeedPower());
-        registerPower(new GodOfCelestialFirePower());
+        registerPower(new SoufflePower());
     }
 
     @Override
@@ -29,74 +24,18 @@ public final class ZenitsuRole extends AbstractRole {
     }
 
     @Override
-    public void onSecond(Player player) {
-        IActionBar actionBar = API.get().getActionBarHandler().getActionBar(player.getUniqueId());
-        actionBar.addActionBar("debug", "Walk Speed: " + player.getWalkSpeed());
+    public String getDescription(Player player) {
+        return "<dark_aqua>»</dark_aqua><st><dark_gray>------------------------------</dark_gray></st><dark_aqua>«</dark_aqua><newline><newline>▪ <dark_aqua><b>Demon Slayer UHC</b></dark_aqua><newline> <dark_gray>❘</dark_gray> <gray>Rôle:</gray> Vous êtes <dark_green>Zenitsu</dark_green>.<newline> <dark_gray>❘</dark_gray> <gray>Camp:</gray> <dark_green>Slayers</dark_green>.<newline><newline> <dark_gray>❘</dark_gray> <gray>Passifs:</gray> Vous possédez l'effet <aqua>Vitesse I</aqua>.<newline>  Vous obtenez l'effet <red>Force I</red> entre <red>7.5❤</red> et <red>4.5❤</red> ainsi que l'effet <aqua>Vitesse II</aqua> entre <red>4❤</red> et <red>0.5❤</red>.<newline><newline> <dark_gray>❘</dark_gray> <dark_green><hover:show_text:\"<dark_green>DIVINE SPEED</dark_green> <dark_gray>≫</dark_gray> <gray>Capacité</gray><newline><newline><dark_gray>[</dark_gray><gray>Clic-droit</gray><dark_gray>]</dark_gray> : Vous permet d'obtenir l'effet <aqua>Vitesse III</aqua> pendant une minute.<newline>Cependant, vous ne pourrez plus recevoir l'effet <red>Force I</red>.<newline>A la fin de ce laps de temps, vous obtenez l'effet <dark_gray>Lenteur II</dark_gray> durant <gray>2 minutes</gray>.<newline><newline>• <gray>Délai d'activation</gray> : <dark_aqua>20 minutes</dark_aqua><newline>• <gray>Utilisations</gray> : <dark_aqua>2</dark_aqua>\">DIVINE SPEED</hover></dark_green> <gray>(</gray><dark_aqua>«</dark_aqua> <gray>Survole</gray> <dark_aqua>»</dark_aqua><gray>)</gray><newline> <dark_gray>❘</dark_gray> <dark_green><hover:show_text:\"<dark_green>GOD OF CELESTIAL FIRE</dark_green> <dark_gray>≫</dark_gray> <gray>Capacité</gray><newline><newline><dark_gray>[</dark_gray><gray>Clic-droit</gray><dark_gray>]</dark_gray> : Vous faites tomber des <yellow>éclairs</yellow> sur tous les joueurs dans un rayon de <gray>30 blocs</gray>,<newline> infligeant <red>2❤</red> de dégâts et les <gold>enflammant</gold> pendant <gray>8 secondes</gray>.<newline><newline>• <gray>Utilisations</gray> : <dark_aqua>1</dark_aqua>\">GOD OF CELESTIAL FIRE</hover></dark_green> <gray>(</gray><dark_aqua>«</dark_aqua> <gray>Survole</gray> <dark_aqua>»</dark_aqua><gray>)</gray><newline><newline><dark_aqua>»</dark_aqua><st><dark_gray>------------------------------</dark_gray></st><dark_aqua>«</dark_aqua>";
     }
 
     @Getter
-    private static final class DivineSpeedPower extends AbstractItemPower {
-
+    private static final class SoufflePower extends AbstractParentPower {
         private final String name;
-        private final int initialCooldown, initialUses;
-        private final ClickType clickType;
 
-        public DivineSpeedPower() {
-            this.name = "&a&lDivine Speed";
-            this.initialCooldown = 20 * 60;
-            this.initialUses = 2;
-            this.clickType = ClickType.RIGHT_CLICK;
-        }
-
-        @Override
-        public ItemStack getIcon() {
-            return new ItemBuilder(Material.GLOWSTONE_DUST)
-                    .name(name)
-                    .glowing(true)
-                    .asItemStack();
-        }
-
-        @Override
-        public boolean onClick(Player player) {
-            IProfile profile = IProfile.of(player.getUniqueId());
-            profile.setSpeedBuffer(profile.getSpeedBuffer() + 0.08F);
-            Bukkit.getScheduler().runTaskLater(DSPlugin.get(), () -> {
-                profile.setSlownessBuffer(0.06F);
-                profile.setSpeedBuffer(profile.getSpeedBuffer() - 0.12F);
-                Bukkit.getScheduler().runTaskLater(DSPlugin.get(), () -> {
-                    profile.setSlownessBuffer(0.0F);
-                    profile.setSpeedBuffer(profile.getSpeedBuffer() + 0.04F);
-                }, 15 * 20L);
-            }, 15 * 20L);
-            return true;
-        }
-    }
-
-    @Getter
-    private static final class GodOfCelestialFirePower extends AbstractItemPower {
-
-        private final String name;
-        private final int initialCooldown, initialUses;
-        private final ClickType clickType;
-
-        public GodOfCelestialFirePower() {
-            this.name = "&a&lGod of celestial fire";
-            this.initialCooldown = 0;
-            this.initialUses = 1;
-            this.clickType = ClickType.RIGHT_CLICK;
-        }
-
-        @Override
-        public ItemStack getIcon() {
-            return new ItemBuilder(Material.BLAZE_POWDER)
-                    .name(name)
-                    .glowing(true)
-                    .asItemStack();
-        }
-
-        @Override
-        public boolean onClick(Player player) {
-            return true;
+        public SoufflePower() {
+            this.name = "&a&lSOUFFLE";
+            registerChild(new DivineSpeedPower());
+            registerChild(new GodOfCelestialFirePower());
         }
     }
 }
