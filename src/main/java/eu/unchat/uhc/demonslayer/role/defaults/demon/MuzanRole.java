504bc98dc619ff6c57cc4ecfaf33513380c53dba
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-10 17:38:08 +0200

    Added Muzan

--- src/main/java/eu/unchat/uhc/demonslayer/role/defaults/demon/MuzanRole.java
+++ src/main/java/eu/unchat/uhc/demonslayer/role/defaults/demon/MuzanRole.java
@@ -1,12 +1,13 @@
 package eu.unchat.uhc.demonslayer.role.defaults.demon;
 
-import eu.unchat.uhc.demonslayer.team.defaults.SlayerTeam;
+import eu.unchat.uhc.demonslayer.team.defaults.DemonTeam;
 import eu.unchat.uhc.power.AbstractPower;
 import eu.unchat.uhc.role.AbstractRole;
 import eu.unchat.uhc.role.Role;
 import lombok.Getter;
+import org.bukkit.Material;
 
-@Role(name = "Muzan", identifier = "muzan", team = SlayerTeam.class)
+@Role(name = "Muzan", identifier = "muzan", team = DemonTeam.class, material = Material.SPIDER_EYE)
 public final class MuzanRole extends AbstractRole {
     public MuzanRole() {
     }
