55f401e17757eaa7ad883eb8b967903edec68578
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-10 17:37:18 +0200

    Added FireSea power

---
+++ src/main/java/eu/unchat/uhc/demonslayer/power/slayer/kyojuro/FireSeaPower.java
@@ -0,0 +1,36 @@
+package eu.unchat.uhc.demonslayer.power.slayer.kyojuro;
+
+import eu.unchat.uhc.power.AbstractItemPower;
+import eu.unchat.uhc.util.ItemBuilder;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+public final class FireSeaPower extends AbstractItemPower {
+
+    private final String name;
+    private final int initialCooldown, initialUses;
+    private final ClickType clickType;
+
+    public FireSeaPower() {
+        this.name = "&a&lFIRE SEA";
+        this.initialCooldown = 10 * 60;
+        this.initialUses = -1;
+        this.clickType = ClickType.RIGHT_CLICK;
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return new ItemBuilder(Material.BLAZE_ROD)
+                .name(name)
+                .glowing(true)
+                .asItemStack();
+    }
+
+    @Override
+    public boolean onClick(Player player) {
+        return true;
+    }
+}
