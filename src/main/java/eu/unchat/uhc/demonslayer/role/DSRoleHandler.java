8e621cc66cc939bdb6fa0e450bc656119ddf9276
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-12 16:40:29 +0200

    Added DSRoleHandler#getTeamList

--- src/main/java/eu/unchat/uhc/demonslayer/role/DSRoleHandler.java
+++ src/main/java/eu/unchat/uhc/demonslayer/role/DSRoleHandler.java
@@ -1,16 +1,30 @@
 package eu.unchat.uhc.demonslayer.role;
 
 import eu.unchat.uhc.API;
-import eu.unchat.uhc.demonslayer.role.defaults.demon.DakiRole;
-import eu.unchat.uhc.demonslayer.role.defaults.demon.KumoRole;
-import eu.unchat.uhc.demonslayer.role.defaults.demon.MuzanRole;
-import eu.unchat.uhc.demonslayer.role.defaults.demon.NakimeRole;
+import eu.unchat.uhc.demonslayer.role.defaults.demon.*;
 import eu.unchat.uhc.demonslayer.role.defaults.slayer.*;
+import eu.unchat.uhc.profile.IProfile;
 import eu.unchat.uhc.role.AbstractRole;
+import eu.unchat.uhc.team.AbstractTeam;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class DSRoleHandler {
     public DSRoleHandler() {
         init();
+    }
+
+    @SafeVarargs
+    public final List<IProfile> getTeamList(final Class<? extends AbstractTeam>... teams) {
+        return API.get()
+                .getProfileHandler()
+                .getAllProfiles()
+                .stream()
+                .filter(profile -> profile.getRole() != null)
+                .filter(profile -> profile.getRole().getTeam() != null)
+                .filter(profile -> Arrays.stream(teams).anyMatch(team -> team.isAssignableFrom(profile.getRole().getTeam())))
+                .toList();
     }
 
     private void init() {
@@ -21,11 +35,13 @@ public final class DSRoleHandler {
         registerRole(GiyuRole.class);
         registerRole(ShinobuRole.class);
         registerRole(KyojuroRole.class);
+        registerRole(ObanaiRole.class);
 
         registerRole(MuzanRole.class);
         registerRole(NakimeRole.class);
         registerRole(KumoRole.class);
         registerRole(DakiRole.class);
+        registerRole(GyutaroRole.class);
     }
 
     private void registerRole(final Class<? extends AbstractRole> clazz) {
