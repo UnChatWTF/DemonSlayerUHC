5e7d6e33c173f7f9eb13ae18fb1743f5257fb209
UnChat <67955616+UnChatWTF@users.noreply.github.com>
2024-08-12 16:37:33 +0200

    Added Daki & Gyutaro

--- src/main/java/eu/unchat/uhc/demonslayer/role/defaults/demon/GyutaroRole.java
+++ src/main/java/eu/unchat/uhc/demonslayer/role/defaults/demon/GyutaroRole.java
@@ -1,32 +1,29 @@
 package eu.unchat.uhc.demonslayer.role.defaults.demon;
 
 import eu.unchat.uhc.demonslayer.death.DakiGyutaroDeathOperation;
-import eu.unchat.uhc.demonslayer.power.demon.daki.ObiSashPower;
+import eu.unchat.uhc.demonslayer.power.demon.gyutaro.BloodManipulationPower;
 import eu.unchat.uhc.demonslayer.team.defaults.DemonTeam;
 import eu.unchat.uhc.profile.IProfile;
 import eu.unchat.uhc.role.AbstractRole;
 import eu.unchat.uhc.role.Role;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 @Getter
-@Role(name = "Daki", identifier = "daki", team = DemonTeam.class, material = Material.BARRIER)
-public final class DakiRole extends AbstractRole {
+@Role(name = "Gyutaro", identifier = "gyutaro", team = DemonTeam.class, material = Material.DIAMOND_HOE)
+public final class GyutaroRole extends AbstractRole {
     private final String tolgeeReference;
 
-    public DakiRole() {
-        this.tolgeeReference = "fr.unchat.demonslayer.role.daki";
+    public GyutaroRole() {
+        this.tolgeeReference = "fr.unchat.demonslayer.role.gyutaro";
 
         registerKnownRole(MuzanRole.class);
-        registerKnownRole(GyutaroRole.class);
-
-        registerPower(new ObiSashPower());
+        registerKnownRole(DakiRole.class);
+        registerPower(new BloodManipulationPower());
     }
 
     @Override
-    @SneakyThrows
     public void onDistribute(Player player) {
         IProfile profile = IProfile.of(player.getUniqueId());
         DakiGyutaroDeathOperation deathOperation = new DakiGyutaroDeathOperation(profile);
